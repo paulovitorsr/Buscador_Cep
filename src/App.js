ef76498a280e617e45715606838b07697d9fbdb5
rosariopv <pv.php2021@gmail.com>
2022-10-03 23:27:51 -0300

    fim do projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {FiSearch} from 'react-icons/fi';
-import './styles.css'; 
+import api from "./services/api";
+import './styles.css';
 
 
 function App() {
+  const [input, SetInput] = useState('');
+  const [cep, SetCep] = useState({});
+
+ 
+    async function handleButton(){
+      if (input === '') {
+        alert('Favor preencher algum cep')
+        return;
+      }
+  
+      try {
+        const response = await api.get(`${input}/json`)
+        SetCep(response.data)
+        SetInput("")
+      } catch{
+        alert("Ops error ao buscar o cep");
+        SetInput('');
+      }
+  
+    }
+
+
   return (
     <div className="container">
       <h2 className="title">Buscador CEP</h2>
@@ -12,21 +35,27 @@ function App() {
         <input 
           type="text" 
           placeholder="Digite seu cep"
+          value={input}
+          onChange={ (e) => SetInput(e.target.value) }
         />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleButton}>
           <FiSearch size={25} color="#fff"/>
         </button>
       </div>
 
-      <main className="main">
-        <h2>CEP: 40391172</h2>
+      {Object.keys(cep).length > 0 &&(
 
-        <span>Rua desembarg j m v de castro</span>
-        <span>Complemento: casa</span>
-        <span>Bairro: Sao caetano</span>
-        <span>Salvador - BA</span>
-      </main>
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+
+          <span>logradouro: {cep.logradouro}</span>
+          <span>Bairro: {cep.bairro}</span>
+          <span>{cep.localidade} - {cep.uf}</span>
+        </main>
+
+      )}
+
     </div>
   );
 }
